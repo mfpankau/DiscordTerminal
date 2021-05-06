bf9880b342ace1ee524da0b01c8fe697117bec5b
mfpankau <74994673+mfpankau@users.noreply.github.com>
2021-05-05 20:41:50 -0400

    Added new functionality, tweaked some stuff

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.Entities;
 using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 namespace BGAppTesting
 {
@@ -19,12 +20,13 @@ namespace BGAppTesting
             MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
             File.WriteAllLines("t.txt", Directory.GetFiles(Directory.GetCurrentDirectory()));
         }
-
         //start bot and stuff
         static async Task MainAsync(string[] args)
         {
+            int writeMode = 0;
+            string fileDir = "";
             //token 
-            //
+            //<Token>
             string data = "";
             //create bot
             discord = new DiscordClient(new DiscordConfiguration
@@ -36,64 +38,118 @@ namespace BGAppTesting
             //if message created
             discord.MessageCreated += async (s, e) =>
             {
-                //ls command
-                if(e.Message.Content.StartsWith("ls"))
+                if (!e.Message.Author.IsBot)
                 {
-                    data = "**Directories**\n";
-                    string[] arr = Directory.GetDirectories(curDir);
-                    for (int i = 0; i < arr.Length; i++)
+                    char[] args = FilterArgs(e.Message.Content).ToArray();
+                    //writing to file
+                    if (writeMode == 1)
                     {
-                        data += arr[i].Replace(curDir, "") + "\n";
+                        Console.WriteLine(fileDir);
+                        File.WriteAllText(fileDir, e.Message.Content);
+                        writeMode = 0;
+                        fileDir = "";
                     }
-                    data += "**Files**\n";
-                    arr = Directory.GetFiles(curDir);
-                    for (int i = 0; i < arr.Length; i++)
+                    /*if(writeMode == 2)
                     {
-                        data += arr[i].Replace(curDir, "") + "\n";
-                    }
-                    discord.SendMessageAsync(e.Message.Channel, data);
-                    data = "";
-                }
-                //cd command
-                if(e.Message.Content.StartsWith("cd"))
-                {
-                    string address = e.Message.Content.Replace("cd ", "");
-                    if(address == "..")
+                        File.WriteAllBytes(fileDir, e.Message.Content);
+                        writeMode = 0;
+                    }*/
+                    //ls command
+                    if (e.Message.Content.StartsWith("ls"))
                     {
-                        RemoveLastDir();
-                    }
-                    else
-                    {
-                        curDir += '\\' + address;
-                    }
-                    discord.SendMessageAsync(e.Message.Channel, "**Current Directory Set to: **" + curDir);
-                }
-                //curDir command
-                if(e.Message.Content.StartsWith("curDir"))
-                {
-                    discord.SendMessageAsync(e.Message.Channel, "**Current Directory is: **" + curDir);
-                }
-                //cat command
-                if(e.Message.Content.StartsWith("cat"))
-                {
-                    data = File.ReadAllText(curDir + '\\' + e.Message.Content.Replace("cat ", ""));
-                    discord.SendMessageAsync(e.Message.Channel, data);
-                    data = "";
-                }
-                //bcat command       cat but for bytes
-                if (e.Message.Content.StartsWith("bcat"))
-                {
-                    byte[] bytes = File.ReadAllBytes(curDir + '\\' + e.Message.Content.Replace("bcat ", ""));
-                    for (int i = 0; i < 500; i++)
-                    {
-                        if(i >= bytes.Length)
+                        data = "**Directories**\n";
+                        string[] arr = Directory.GetDirectories(curDir);
+                        for (int i = 0; i < arr.Length; i++)
                         {
-                            break;
+                            data += arr[i].Replace(curDir, "") + "\n";
                         }
-                        data += bytes[i].ToString();
+                        data += "**Files**\n";
+                        arr = Directory.GetFiles(curDir);
+                        for (int i = 0; i < arr.Length; i++)
+                        {
+                            data += arr[i].Replace(curDir, "") + "\n";
+                        }
+                        discord.SendMessageAsync(e.Message.Channel, data);
+                        data = "";
                     }
-                    discord.SendMessageAsync(e.Message.Channel, "**Length of file in bytes: **" + bytes.Length.ToString() + '\n' + "**First 500 bytes of data: **" + data);
-                    data = "";
+                    //cd command
+                    if (e.Message.Content.StartsWith("cd"))
+                    {
+                        string address = e.Message.Content.Replace("cd ", "");
+                        if (address == "..")
+                        {
+                            RemoveLastDir();
+                        }
+                        else
+                        {
+                            curDir += '\\' + address;
+                        }
+                        discord.SendMessageAsync(e.Message.Channel, "**Current Directory Set to: **" + curDir);
+                    }
+                    //curDir command
+                    if (e.Message.Content.StartsWith("curDir"))
+                    {
+                        discord.SendMessageAsync(e.Message.Channel, "**Current Directory is: **" + curDir);
+                    }
+                    //cat command
+                    if (e.Message.Content.StartsWith("cat"))
+                    {
+                        data = File.ReadAllText(curDir + '\\' + e.Message.Content.Replace("cat ", ""));
+                        discord.SendMessageAsync(e.Message.Channel, data);
+                        data = "";
+                    }
+                    //bcat command       cat but for bytes
+                    if (e.Message.Content.StartsWith("bcat"))
+                    {
+                        byte[] bytes = File.ReadAllBytes(curDir + '\\' + e.Message.Content.Replace("bcat ", ""));
+                        for (int i = 0; i < 500; i++)
+                        {
+                            if (i >= bytes.Length)
+                            {
+                                break;
+                            }
+                            data += bytes[i].ToString();
+                        }
+                        discord.SendMessageAsync(e.Message.Channel, "**Length of file in bytes: **" + bytes.Length.ToString() + '\n' + "**First 500 bytes of data: **" + data);
+                        data = "";
+                    }
+                    //testing, REMOVE
+                    if (e.Message.Content.StartsWith("test"))
+                    {
+                        for (int i = 0; i < args.Length; i++)
+                        {
+                            data += args[i] + "\n";
+                        }
+                        discord.SendMessageAsync(e.Message.Channel, data);
+                        data = "";
+                    }
+                    //touch command
+                    if (e.Message.Content.StartsWith("touch"))
+                    {
+                        //deal with issues
+                        if (args.Length < 1)
+                        {
+                            discord.SendMessageAsync(e.Message.Channel,
+                                "**Incorrect Usage!**\n" +
+                                "**Please input the command in the form:**\n" +
+                                "touch !<write as(!t, !b)> !<access parameter(!w, !a)> <name of new file>\n" +
+                                "Please note: NEW FILE CAN ONLY BE CREATED IN CURRENT WORKING DIRECTORY");
+                        }
+                        else
+                        {
+                            if (args[0] == 'b')
+                            {
+                                writeMode = 2; //write as byte
+                                fileDir = curDir + "\\" + e.Message.Content.Replace("touch !b ", "");
+                            }
+                            else if (args[0] == 't')
+                            {
+                                writeMode = 1; //write as plaintext
+                                fileDir = curDir + "\\" + e.Message.Content.Replace("touch !t ", "");
+                            }
+                            discord.SendMessageAsync(e.Message.Channel, "**Next message will be written to file in selected mode.**");
+                        }
+                    }
                 }
             };
 
@@ -107,12 +163,25 @@ namespace BGAppTesting
             char[] chars = curDir.ToCharArray();
             for (int i = chars.Length - 1; i >= 0; i--)
             {
-                if(chars[i] == '\\')
+                if (chars[i] == '\\')
                 {
                     curDir = curDir.Substring(0, i);
                     return;
                 }
             }
+        }
+        static List<char> FilterArgs(string input)
+        {
+            char[] chars = input.ToCharArray();
+            List<char> args = new List<char>();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if(chars[i] == '!')
+                {
+                    args.Add(chars[i + 1]);
+                }
+            }
+            return args;
         }
     }
 }
